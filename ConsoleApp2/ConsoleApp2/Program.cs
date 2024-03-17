819d50209dde2cd7433065e91ba6a78b1badf1b5
s12699 <s12699@pjwstk.edu.pl>
2024-03-17 22:29:13 +0100

    Static function added. It counts average value from array

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -4,3 +4,24 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Ola, Mundo!");
 Console.WriteLine("Witaj, swiecie!");
 Console.WriteLine("Hei maailma!");
+
+int[] testVal= [2, 5, 5, 6, 4, 3];
+
+static int average(int[] ave)
+{
+    int sum = 0;
+    int counter = 0;
+    
+    for (int i = 0; i < ave.Length; i++)
+    {
+        sum += ave[i];
+        counter++;
+
+    }
+
+    int val = sum / counter;
+    
+    return val;
+}
+
+Console.WriteLine(average(testVal));
