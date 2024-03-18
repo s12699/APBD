94120620512335958be95983caf725ca5aed6bef
s12699 <s12699@pjwstk.edu.pl>
2024-03-18 20:50:55 +0100

    Change variable name inside the loop in the average function

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -6,16 +6,16 @@ Console.WriteLine("Witaj, swiecie!");
 Console.WriteLine("Hei maailma!");
 
 int[] testVal= [2, 5, 5, 6, 4, 3];
-int[] testVal2 = [1, 16, 5345, 9, 0, 23424, 42];
+int[] testVal2 = [1, 16, 53451, 9, 0, 23424, 42];
 
 static int average(int[] ave)
 {
     int sum = 0;
     int counter = 0;
     
-    for (int i = 0; i < ave.Length; i++)
+    for (int inc = 0; inc < ave.Length; inc++)
     {
-        sum += ave[i];
+        sum += ave[inc];
         counter++;
 
     }
