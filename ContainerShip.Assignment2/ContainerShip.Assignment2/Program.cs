85b493baa252853d2662258ae411ce5edd27da02
s12699 <s12699@pjwstk.edu.pl>
2024-03-30 23:31:37 +0100

    Sloving problem with LoadinToShip, ShipInfo methods. RemoveContainer method has been added

--- ContainerShip.Assignment2/ContainerShip.Assignment2/Program.cs
+++ ContainerShip.Assignment2/ContainerShip.Assignment2/Program.cs
@@ -10,15 +10,18 @@ public class ContainerShip
         RefrigeratedContainer ald = new RefrigeratedContainer(2, 3, 4, 100);
         Container con = new Container(1, 2, 15, 10);
         LiquidContainer lc = new LiquidContainer(3, 4, 5, 100);
-        GasContainer gc = new GasContainer(5, 5, 15, 100);
+        GasContainer gc = new GasContainer(5, 5, 99, 100);
 
         /*ald.DisplayInfo();
         con.DisplayInfo();
         lc.DisplayInfo();
         gc.DisplayInfo();*/
 
-        Ship spgShip = new Ship(150, 200, 1000);
+        Ship spgShip = new Ship(150, 2, 1);
+        spgShip.LoadToShip(gc);
         spgShip.LoadToShip(ald);
+        spgShip.ShipInfo();
+        spgShip.RemoveContainer(gc);
         spgShip.ShipInfo();
 
     }
--- ContainerShip.Assignment2/ContainerShip.Assignment2/Ship.cs
+++ ContainerShip.Assignment2/ContainerShip.Assignment2/Ship.cs
@@ -2,21 +2,23 @@ namespace ContainerShip.Assignment2;
 
 public class Ship
 {
-    private HashSet<Container> _containers;
+    private Dictionary<String, Container> _containers;
     
     //Max Speed in knots
     private int _maxSpeed;
 
     //Max possible number of containers to load on board
     private int _maxContainersNumber;
+    private int _currentContainersNumber;
 
     //Max weight of Cargo to load
     //Max Cargo Weight in tons
-    private int _maxCargoWeight;
+    private double _maxCargoWeight;
+    private double _currentCargoWeight;
 
     public Ship(/*HashSet<Container> containers, */int maxSpeed, int maxContainersNumber, int maxCargoWeight)
     {
-        _containers = new HashSet<Container>();
+        _containers = new Dictionary<String, Container>();
         _maxSpeed = maxSpeed;
         _maxContainersNumber = maxContainersNumber;
         _maxCargoWeight = maxCargoWeight;
@@ -25,18 +27,58 @@ public class Ship
 
     public void LoadToShip(Container con)
     {
+
+        if ((_currentContainersNumber < _maxContainersNumber) && ((_maxCargoWeight - _currentCargoWeight) > ((con.tareWeight+con.cargoMass)*0.001)))
+        {
+            _containers.Add(con.serialNumber, con);
+            _currentContainersNumber++;
+            _currentCargoWeight = Math.Round((_currentCargoWeight + ((con.tareWeight + con.cargoMass) * 0.001)), 3);
+            
+            Console.WriteLine("Shipping container: " + con.serialNumber);
+            
+        }
+        else
+        {
+            Console.WriteLine("You cannot load following container onto the ship: " + con.serialNumber);
+        }
         
-        _containers.Add(con);
-        //return _containers;
     }
 
     public void ShipInfo()
     {
-       
-        foreach(var i in _containers)
+        Console.WriteLine("************* SHIP INFORMATION ***************");
+        Console.WriteLine("Max speed: " + _maxSpeed + " Knots");
+        Console.WriteLine("Max weight of cargo: " + _maxCargoWeight + " Tons");
+        Console.WriteLine("Current weight of cargo: " + _currentCargoWeight + " Tons");
+        Console.WriteLine("Max number of containers: " + _maxContainersNumber);
+        Console.WriteLine("Current number of containers: " + _currentContainersNumber);
+        Console.WriteLine("Ship carry following containers: ");
+        
+        foreach (string key in _containers.Keys)
         {
-            Console.WriteLine(i);
+            Console.WriteLine(key);
         }
+        //_containers.ToString();
+        
+        //Console.WriteLine(_containers["KON--2"].DisplayInfo());
+        //_containers["KON--2"].DisplayInfo();
+        
+    }
+
+    public void RemoveContainer(Container con)
+    {
+        if (_containers.ContainsKey(con.serialNumber))
+        {
+            Console.WriteLine("Removing container: " + con.serialNumber + " from the ship");
+            _containers.Remove(con.serialNumber);
+            _currentContainersNumber--;
+            _currentCargoWeight = Math.Round((_currentCargoWeight - ((con.tareWeight + con.cargoMass) * 0.001)), 3);
+        }
+        else
+        {
+            Console.WriteLine("Given container: " + con.serialNumber + " is not on the ship");
+        }
+
         
     }
     
